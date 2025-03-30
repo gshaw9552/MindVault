118574a04f2cbdc15f547689016f467341d66652
gshaw9552 <gshaw9552@gmail.com>
2025-03-31 01:49:19 +0530

    created a generic card component

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,54 @@
+import { DeleteIcon } from "../icons/DeleteIcon";
+import { DocumentIcon } from "../icons/DocumentIcon";
+import { ShareIcon } from "../icons/ShareIcon";
+// import TweetEmbed from "react-tweet-embed";
+
+interface CardProps {
+  title: string;
+  link: string;
+  type: "youtube" | "twitter";
+}
+
+export function Card({ title, link, type }: CardProps) {
+  return (
+    <div>
+      <div className="p-8 bg-white rounded-md border-gray-200 border max-w-96 min-h-48 min-w-72">
+        <div className="flex justify-between">
+          <div className="flex items-center">
+            <div className="text-gray-500 pr-2">
+              <DocumentIcon />
+            </div>
+            {title}
+          </div>
+          <div className="text-gray-500 flex items-center">
+            <div className="pr-2">
+              <a href={link} target="_blank">
+                <ShareIcon />
+              </a>
+            </div>
+            <div>
+              <DeleteIcon />
+            </div>
+          </div>
+        </div>
+        <div className="pt-4">
+          {type === "youtube" && (
+            <iframe
+              className="w-full"
+              src={`https://www.youtube.com/embed/${new URL(link).searchParams.get("v")}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          )}
+          {type === "twitter" && <blockquote className="twitter-tweet">
+            <a href={link.replace("x.com", "twitter.com")}></a>
+          </blockquote>}
+          {/* <TweetEmbed id= "1866815354153861341" /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
