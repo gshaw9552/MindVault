fad663b97cb315a2b05e11614b2d5a9387c22d16
gshaw9552 <gshaw9552@gmail.com>
2025-03-31 02:37:16 +0530

    added routes for pages in React App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
+import Dashboard from './pages/Dashboard'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
-
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+  
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>   
+  </BrowserRouter>
+  
 }
-
+// 2:34
 export default App
+
+
+// react-hook-forms
+// react-query
